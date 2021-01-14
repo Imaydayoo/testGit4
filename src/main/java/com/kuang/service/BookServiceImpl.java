2e6266f41064ea93128adfc679158462edb7db90
mayday <1944680775@qq.com>
2021-01-14 16:40:48 +0800

    test1

--- src/main/java/com/kuang/service/BookServiceImpl.java
+++ src/main/java/com/kuang/service/BookServiceImpl.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class BookServiceImpl implements BookService{
     //调用dao层的操作，设置一个set接口，方便Spring管理
-    private BookMapper bookMapper;
+    private BookMapper bookMapper;//
 
     public int addBook(Books book) {
         return bookMapper.addBook(book);
